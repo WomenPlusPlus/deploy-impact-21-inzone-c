6321ee15ca35742f652321c0f1467c3d6fd079b7
ogimgio <ogimgamers@gmail.com>
2021-10-26 21:38:49 +0200

    added sign up page

--- client-inzone/src/App.js
+++ client-inzone/src/App.js
@@ -1,18 +1,29 @@
 //import { useTranslation } from 'react-i18next';
 import Login from "./components/login";
+import SignUp from "./components/signUp"
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Page1 from './components/Page1';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import red from '@mui/material/colors/red';
+
+const theme = createTheme({
+    palette: {
+        primary: red,
+    },
+});
 
 function App() {
     //const { t} = useTranslation();
   return (
     <div className="App">
     <Router>
+        <ThemeProvider theme={theme}>
         <div className="Title">
             <h1>InZone</h1>
         </div>
-        <Login/>
+        <SignUp/>
         <Route path={'/page1'} component={Page1}/>
+        </ThemeProvider>
     </Router>
     </div>
   );

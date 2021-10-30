f6b4836fa3632417c6546f717140f637014c9e08
emrec <iletisim@paradyo.com>
2021-10-30 14:32:10 +0300

    templates of home and notifications

--- client-inzone/src/components/Student/HomeScreen.js
+++ client-inzone/src/components/Student/HomeScreen.js
@@ -10,12 +10,109 @@ import {
   IconButton,
   ListItemText,
   List,
+  Stepper,
+  Step,
+  StepLabel,
+  StepContent,
+  Button,
+  Paper,
 } from "@mui/material";
 import { Assignment, PlayCircleFilledWhite } from "@mui/icons-material";
 
+const steps = [
+  {
+    label: "Complete Your Profile and Settings",
+    description: `Explanation about completing the profile and settings like notification.`,
+  },
+  {
+    label: "Learn How To Use the Application",
+    description:
+      "Teach how to use the Application.",
+  },
+  {
+    label: "See Your Progress",
+    description: `Let's show how the student will look at their results.`,
+  },
+  {
+    label: "Chat With Friends",
+    description: `Chatting usability.`,
+  },
+  {
+    label: "Step 5",
+    description:
+      "Step 5 Description",
+  },
+  {
+    label: "Step 6",
+    description: `Step 6 Description`,
+  },
+  {
+    label: "Step 7",
+    description: `Step 7 Description`,
+  },
+];
 const HomeScreen = () => {
-  
-  return "Home";
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+  return (
+    <Box sx={{ padding: '10%',  }}>
+      <Stepper activeStep={activeStep} orientation="vertical">
+        {steps.map((step, index) => (
+          <Step key={step.label}>
+            <StepLabel
+              optional={
+                index === 2 ? (
+                  <Typography variant="caption">Last step</Typography>
+                ) : null
+              }
+            >
+              {step.label}
+            </StepLabel>
+            <StepContent>
+              <Typography>{step.description}</Typography>
+              <Box sx={{ mb: 2 }}>
+                <div>
+                  <Button
+                    variant="contained"
+                    onClick={handleNext}
+                    sx={{ mt: 1, mr: 1 }}
+                  >
+                    {index === steps.length - 1 ? "Finish" : "Continue"}
+                  </Button>
+                  <Button
+                    disabled={index === 0}
+                    onClick={handleBack}
+                    sx={{ mt: 1, mr: 1 }}
+                  >
+                    Back
+                  </Button>
+                </div>
+              </Box>
+            </StepContent>
+          </Step>
+        ))}
+      </Stepper>
+      {activeStep === steps.length && (
+        <Paper square elevation={0} sx={{ p: 3 }}>
+          <Typography>All steps completed - you&apos;re finished</Typography>
+          <Button onClick={handleReset} sx={{ mt: 1, mr: 1 }}>
+            Reset
+          </Button>
+        </Paper>
+      )}
+    </Box>
+  );
 };
 
 export default HomeScreen;
--- client-inzone/src/components/Student/NotificationScreen.js
+++ client-inzone/src/components/Student/NotificationScreen.js
@@ -1,8 +1,78 @@
 import React, { Component } from "react";
-import { Typography } from "@mui/material";
+import {
+  Typography,
+  Box,
+  ListItem,
+  ListItemButton,
+  ListItemAvatar,
+  Divider,
+  Avatar,
+  IconButton,
+  ListItemText,
+  List,
+} from "@mui/material";
+import { Assignment, Check, CircleNotifications, PlayCircleFilledWhite } from "@mui/icons-material";
 
 const NotificationScreen = () => {
-  return <Typography>Notifications</Typography>;
+  const renderRow = () => {
+    return (
+      <>
+        <ListItem
+          alignItems="flex-start"
+          secondaryAction={
+            <IconButton edge="end" aria-label="delete">
+              <Check />
+            </IconButton>
+          }
+        >
+          <ListItemAvatar>
+            <Avatar>
+              <CircleNotifications />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="Exam Notification!" secondary={"Notification Description"} />
+        </ListItem>
+        <Divider variant="inset" component="li" />
+        <ListItem
+          alignItems="flex-start"
+          secondaryAction={
+            <IconButton edge="end" aria-label="delete">
+              <Check />
+            </IconButton>
+          }
+        >
+          <ListItemAvatar>
+            <Avatar>
+              <CircleNotifications />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="Chat Notification" secondary={"Notification Description"} />
+        </ListItem>
+        <Divider variant="inset" component="li" />
+        <ListItem
+          alignItems="flex-start"
+          secondaryAction={
+            <IconButton edge="end" aria-label="delete">
+              <Check />
+            </IconButton>
+          }
+        >
+          <ListItemAvatar>
+            <Avatar>
+              <CircleNotifications />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="Certificate Notification" secondary={"Notification Description"} />
+        </ListItem>
+        <Divider variant="inset" component="li" />
+      </>
+    );
+  };
+  return (
+    <List sx={{ width: "100%", bgcolor: "background.paper" }}>
+      {renderRow()}
+    </List>
+  );
 };
 
 export default NotificationScreen;

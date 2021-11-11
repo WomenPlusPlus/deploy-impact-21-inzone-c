6ca758e570a13dc7dd35b1fbb6031b9338aeb99d
GiadaFallo <giada.fallo@gmail.com>
2021-11-11 08:11:27 +0100

    added toggle to switch the theme and converted the navbar in a functional based component

--- client-inzone/src/App.js
+++ client-inzone/src/App.js
@@ -1,31 +1,12 @@
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import React, { useState } from "react";
+import React from 'react'
 import createRoutes from "./components/Routes";
-
-export const light = {
-  palette: {
-    mode: 'light',
-  },
-}
-export const dark = {
-  palette: {
-    mode: 'dark',
-  },
-}
 
 function App() {
 
-  // const [theme, setTheme] = useState(true);
-  // const icon = !theme ? <Brightness7Outlined /> : <Brightness3Outlined /> 
-  const appliedTheme = createTheme(light);
-
   return (
-
-    <ThemeProvider theme={appliedTheme}>
-      <div className="App">
-        {createRoutes()}
-      </div>
-    </ThemeProvider>
+    <div className="App">
+      {createRoutes()}
+    </div>
   );
 }
 
--- client-inzone/src/components/Navbar.js
+++ client-inzone/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useContext, useEffect } from "react";
 import {
   Box,
   CssBaseline,
@@ -15,120 +15,140 @@ import {
   Notifications,
   Assignment,
 } from "@mui/icons-material";
-import { withRouter } from "react-router-dom";
+import { withRouter, useHistory } from "react-router-dom";
+import { CustomThemeContext } from '../themes/CustomThemeProvider'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import SwitchUI from '@material-ui/core/Switch'
 
-class Navbar extends Component {
-  componentDidMount() {
+
+function Navbar() {
+  const { currentTheme, setTheme } = useContext(CustomThemeContext)
+  const history = useHistory();
+
+  useEffect(() => {
     if (!JSON.parse(localStorage.getItem("userInformation"))) {
-      this.props.history.push("/login");
+      history.push("/login");
       window.location.reload(false)
     }
+  });
+
+  const isDark = Boolean(currentTheme === 'dark')
+
+  const handleThemeChange = (event) => {
+    const { checked } = event.target
+    if (checked) {
+      setTheme('dark')
+    } else {
+      setTheme('normal')
+    }
   }
-  render() {
-    return (
-      <Box sx={{ pb: 7 }}>
-        <CssBaseline />
-        {/* app bar */}
-        <AppBar position="static">
-          <Toolbar>
-            <Typography
-              variant="h6"
-              noWrap
-              component="div"
-              sx={{
-                textAlign: "left",
-                flexGrow: 1,
-                display: { xs: "block", sm: "block" },
-              }}
-            >
-              Examify
-            </Typography>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="open drawer"
-              onClick={() =>
-                JSON.parse(localStorage.getItem("userInformation")).role ===
+
+  return (
+    <Box sx={{ pb: 7 }}>
+      <CssBaseline />
+      {/* app bar */}
+      <AppBar position="static">
+        <Toolbar>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{
+              textAlign: "left",
+              flexGrow: 1,
+              display: { xs: "block", sm: "block" },
+            }}
+          >
+            Examify
+          </Typography>
+          <FormControlLabel
+            control={<SwitchUI checked={isDark} onChange={handleThemeChange} />}
+          />
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={() =>
+              JSON.parse(localStorage.getItem("userInformation")).role ===
                 "student"
-                  ? this.props.history.push("/student")
-                  : this.props.history.push("/coordinator")
-              }
-            >
-              <Home />
-            </IconButton>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="open drawer"
-              onClick={() =>
-                JSON.parse(localStorage.getItem("userInformation")).role ===
+                ? history.push("/student")
+                : history.push("/coordinator")
+            }
+          >
+            <Home />
+          </IconButton>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={() =>
+              JSON.parse(localStorage.getItem("userInformation")).role ===
                 "student"
-                  ? this.props.history.push("/student/chat")
-                  : this.props.history.push("/coordinator/chat")
-              }
-            >
-              <Chat />
-            </IconButton>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="open drawer"
-              onClick={() =>
-                JSON.parse(localStorage.getItem("userInformation")).role ===
+                ? history.push("/student/chat")
+                : history.push("/coordinator/chat")
+            }
+          >
+            <Chat />
+          </IconButton>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={() =>
+              JSON.parse(localStorage.getItem("userInformation")).role ===
                 "student"
-                  ? this.props.history.push("/student/notifications")
-                  : this.props.history.push("/coordinator/notifications")
-              }
-            >
-              <Notifications />
-            </IconButton>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="open drawer"
-              onClick={() =>
-                JSON.parse(localStorage.getItem("userInformation")).role ===
+                ? history.push("/student/notifications")
+                : history.push("/coordinator/notifications")
+            }
+          >
+            <Notifications />
+          </IconButton>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={() =>
+              JSON.parse(localStorage.getItem("userInformation")).role ===
                 "student"
-                  ? this.props.history.push("/student/exams")
-                  : this.props.history.push("/coordinator/exams")
-              }
-            >
-              <Assignment />
-            </IconButton>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="open drawer"
-              onClick={() =>
-                JSON.parse(localStorage.getItem("userInformation")).role ===
+                ? history.push("/student/exams")
+                : history.push("/coordinator/exams")
+            }
+          >
+            <Assignment />
+          </IconButton>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={() =>
+              JSON.parse(localStorage.getItem("userInformation")).role ===
                 "student"
-                  ? this.props.history.push("/student/settings")
-                  : this.props.history.push("/coordinator/settings")
-              }
-            >
-              <Settings />
-            </IconButton>
-            <IconButton
-              size="large"
-              color="inherit"
-              aria-label="open drawer"
-              onClick={() => {
-                localStorage.removeItem("userInformation");
-                this.props.history.push("/login");
-                window.location.reload(false)
-              }}
-            >
-              <Logout />
-            </IconButton>
-          </Toolbar>
-        </AppBar>
-      </Box>
-    );
-  }
+                ? history.push("/student/settings")
+                : history.push("/coordinator/settings")
+            }
+          >
+            <Settings />
+          </IconButton>
+          <IconButton
+            size="large"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={() => {
+              localStorage.removeItem("userInformation");
+              this.props.history.push("/login");
+              window.location.reload(false)
+            }}
+          >
+            <Logout />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
 export default withRouter(Navbar);

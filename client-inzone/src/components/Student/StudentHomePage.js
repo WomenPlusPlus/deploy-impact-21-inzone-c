33f2dd08ebfa051b140f1794af58c738b010f327
Juliet879 <juliet.gisemba@amarulasolutions.com>
2021-11-11 17:27:46 +0300

    add navbar styles

--- client-inzone/src/components/Student/StudentHomePage.js
+++ client-inzone/src/components/Student/StudentHomePage.js
@@ -4,24 +4,27 @@ import React from "react";
 
 const StudentHomePage = () => {
   return (
-    <div>
+    <div className="Title">
       <Grid container spacing={2}>
         <Grid item xs={12}>
-          <Item>
+          <Item  style={styles.home}>
             <Typography variant="h2">
               Welcome {JSON.parse(localStorage.getItem("userInformation")).name}{" "}
               !
-            </Typography>
+            </Typography>{" "}
             <Paper variant="outlined">
               <img width="400" src="/home-logo-sample.png" />
-            </Paper>
-            <Typography variant="h4">Let's edit your profile first!</Typography>
+            </Paper>{" "}
             <Typography variant="h4">
-              <Link href="/student/settings"> Click Here!</Link>
-            </Typography>
-          </Item>
-        </Grid>
-      </Grid>
+              {" "}
+              Let 's edit your profile first!
+            </Typography>{" "}
+            <Typography variant="h4">
+              <Link href="/student/settings">Click Here! </Link>{" "}
+            </Typography>{" "}
+          </Item>{" "}
+        </Grid>{" "}
+      </Grid>{" "}
     </div>
   );
 };
@@ -31,4 +34,10 @@ const Item = styled(Paper)(({ theme }) => ({
   textAlign: "center",
   color: theme.palette.text.primary,
 }));
+
+const styles = {
+  home:{
+    
+  }
+}
 export default StudentHomePage;

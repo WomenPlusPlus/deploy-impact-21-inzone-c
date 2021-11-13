22d65b407dd80048302088ed9a30ff9fb35982cb
GiadaFallo <giada.fallo@gmail.com>
2021-11-13 18:00:49 +0100

    merge main brange into styles

--- client-inzone/src/components/Navbar.js
+++ client-inzone/src/components/Navbar.js
@@ -1,7 +1,6 @@
-import React, { Component } from "react";
+import React  from "react";
 import {
   Box,
-  CssBaseline,
   AppBar,
   Toolbar,
   Typography,
@@ -15,144 +14,168 @@ import {
   Notifications,
   Assignment,
 } from "@mui/icons-material";
-import { withRouter } from "react-router-dom";
+import { withRouter, useHistory } from "react-router-dom";
+import { CustomThemeContext } from '../themes/CustomThemeProvider'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import SwitchUI from '@material-ui/core/Switch'
 
-class Navbar extends Component {
-  componentDidMount() {
-    if (!JSON.parse(localStorage.getItem("userInformation"))) {
-      this.props.history.push("/login");
-      window.location.reload(false);
+
+
+function Navbar() {
+  const { currentTheme, setTheme } = useContext(CustomThemeContext);
+  const history = useHistory();
+  const isDark = Boolean(currentTheme === 'dark');
+
+  const styles = {
+    navbar: {
+      //  backgroundColor:"black"
+    },
+    navIcon: {
+      marginRight: 40,
+    },
+    logOutIcon: {
+      marginRight: 200,
+    },
+    title: {
+      marginLeft: 200,
+      fontWeight: 700,
     }
   }
-  render() {
-    return (
-      <Box sx={{ pb: 7 }}>
-        <CssBaseline />
-        {/* app bar */}
-        <AppBar style={styles.navbar} position="static">
-          <Toolbar>
-            <Typography
-              style={styles.title}
-              variant="h6"
-              noWrap
-              component="div"
-              sx={{
-                textAlign: "left",
-                flexGrow: 1,
-                display: { xs: "block", sm: "block" },
-              }}
-            >
-              Examify
-            </Typography>
 
-            <IconButton
-              style={styles.navIcon}
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="open drawer"
-              onClick={() =>
-                JSON.parse(localStorage.getItem("userInformation")).role ===
-                "student"
-                  ? this.props.history.push("/student")
-                  : this.props.history.push("/coordinator")
-              }
-            >
-              <Home />
-            </IconButton>
-            <IconButton
-              style={styles.navIcon}
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="open drawer"
-              onClick={() =>
-                JSON.parse(localStorage.getItem("userInformation")).role ===
-                "student"
-                  ? this.props.history.push("/student/chat")
-                  : this.props.history.push("/coordinator/chat")
-              }
-            >
-              <Chat />
-            </IconButton>
-            <IconButton
-              style={styles.navIcon}
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="open drawer"
-              onClick={() =>
-                JSON.parse(localStorage.getItem("userInformation")).role ===
-                "student"
-                  ? this.props.history.push("/student/notifications")
-                  : this.props.history.push("/coordinator/notifications")
-              }
-            >
-              <Notifications />
-            </IconButton>
-            <IconButton
-              style={styles.navIcon}
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="open drawer"
-              onClick={() =>
-                JSON.parse(localStorage.getItem("userInformation")).role ===
-                "student"
-                  ? this.props.history.push("/student/exams")
-                  : this.props.history.push("/coordinator/exams")
-              }
-            >
-              <Assignment />
-            </IconButton>
-            <IconButton
-              style={styles.navIcon}
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="open drawer"
-              onClick={() =>
-                JSON.parse(localStorage.getItem("userInformation")).role ===
-                "student"
-                  ? this.props.history.push("/student/settings")
-                  : this.props.history.push("/coordinator/settings")
-              }
-            >
-              <Settings />
-            </IconButton>
-            <IconButton
-              style={styles.logOutIcon}
-              size="large"
-              color="inherit"
-              aria-label="open drawer"
-              onClick={() => {
-                localStorage.removeItem("userInformation");
-                this.props.history.push("/login");
-                window.location.reload(false);
-              }}
-            >
-              <Logout />
-            </IconButton>
-          </Toolbar>
-        </AppBar>
-      </Box>
-    );
+  useEffect(() => {
+    if (!JSON.parse(localStorage.getItem("userInformation"))) {
+      history.push("/login");
+      window.location.reload(false)
+    }
+  }, []);
+
+  const handleThemeChange = (event) => {
+    const { checked } = event.target
+    if (checked) {
+      setTheme('dark')
+    } else {
+      setTheme('normal')
+    }
   }
+
+  return (
+    <Box sx={{ pb: 7 }}>
+      {/* app bar */}
+      <AppBar style={styles.navbar} position="static">
+        <Toolbar>
+          <Typography
+            style={styles.title}
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{
+              textAlign: "left",
+              flexGrow: 1,
+              display: { xs: "block", sm: "block" },
+            }}
+          >
+            Examify
+          </Typography>
+          <IconButton
+            style={styles.navIcon}>
+            <FormControlLabel
+
+              control={<SwitchUI checked={isDark} onChange={handleThemeChange} />}
+            />
+          </IconButton>
+          <IconButton
+            style={styles.navIcon}
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={() =>
+              JSON.parse(localStorage.getItem("userInformation")).role ===
+                "student"
+                ? history.push("/student")
+                : history.push("/coordinator")
+            }
+          >
+            <Home />
+          </IconButton>
+          <IconButton
+            style={styles.navIcon}
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={() =>
+              JSON.parse(localStorage.getItem("userInformation")).role ===
+                "student"
+                ? history.push("/student/chat")
+                : history.push("/coordinator/chat")
+            }
+          >
+            <Chat />
+          </IconButton>
+          <IconButton
+            style={styles.navIcon}
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={() =>
+              JSON.parse(localStorage.getItem("userInformation")).role ===
+                "student"
+                ? history.push("/student/notifications")
+                : history.push("/coordinator/notifications")
+            }
+          >
+            <Notifications />
+          </IconButton>
+          <IconButton
+            style={styles.navIcon}
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={() =>
+              JSON.parse(localStorage.getItem("userInformation")).role ===
+                "student"
+                ? history.push("/student/exams")
+                : history.push("/coordinator/exams")
+            }
+          >
+            <Assignment />
+          </IconButton>
+          <IconButton
+            style={styles.navIcon}
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={() =>
+              JSON.parse(localStorage.getItem("userInformation")).role ===
+                "student"
+                ? history.push("/student/settings")
+                : history.push("/coordinator/settings")
+            }
+          >
+            <Settings />
+          </IconButton>
+          <IconButton
+            style={styles.logOutIcon}
+            size="large"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={() => {
+              localStorage.removeItem("userInformation");
+              history.push("/login");
+              window.location.reload(false)
+            }}
+          >
+            <Logout />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+
 }
 
-const styles = {
-  navbar: {
-    //  backgroundColor:"black"
-  },
-  navIcon: {
-    marginRight: 40,
-  },
-  logOutIcon: {
-    marginRight: 200,
-  },
-  title: {
-    marginLeft: 200,
-    fontWeight: 700,
-  },
-};
 export default withRouter(Navbar);
